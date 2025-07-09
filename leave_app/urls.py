e96ecb036fef7e16bb3238981436f2dc1385c455
sridharshini12 <sridharshini1208@gmail.com>
2025-07-09 21:07:09 +0530

    Add deployment files

--- leave_app/urls.py
+++ leave_app/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path('submit/', views.submit_leave_request, name='submit_leave'),
     path('requests/', views.view_leave_requests, name='view_requests'),
     path('login/', auth_views.LoginView.as_view(template_name='leave_app/login.html'), name='login'),
-   
+    path('approve/<int:leave_id>/', views.approve_leave, name='approve_leave'),
+    path('reject/<int:leave_id>/', views.reject_leave, name='reject_leave'),
 ]
 
 
--- leave_app/views.py
+++ leave_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import LeaveRequest
 from .forms import LeaveRequestForm
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
 
 # 👇 This view handles the leave request form submission
 @login_required
@@ -22,4 +23,16 @@ def submit_leave_request(request):
 def view_leave_requests(request):
     leaves = LeaveRequest.objects.all()
     return render(request, 'leave_app/view_requests.html', {'leaves': leaves})
+@login_required
+def approve_leave(request, leave_id):
+    leave = get_object_or_404(LeaveRequest, id=leave_id)
+    leave.status = 'Approved'
+    leave.save()
+    return redirect('view_requests')
 
+@login_required
+def reject_leave(request, leave_id):
+    leave = get_object_or_404(LeaveRequest, id=leave_id)
+    leave.status = 'Rejected'
+    leave.save()
+    return redirect('view_requests')
